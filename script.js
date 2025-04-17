8164c0b58cf54d06c21198557c43fd974b7cad7a
Gianni <132337749+giannindm@users.noreply.github.com>
2025-04-17 21:26:48 +0200

    Fix multiple prompts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid");
 const clear = document.querySelector(".clear");
 const newGrid = document.querySelector(".new-grid");
+let squares;
 
 function renderGrid(numSquares) {
     for (let i = 0; i < numSquares**2; i++) {
@@ -10,26 +11,30 @@ function renderGrid(numSquares) {
         square.style.width = `${squareWidth}px`;
         grid.appendChild(square);
     };
-    
-    const squares = document.querySelectorAll(".square");
+
+    squares = document.querySelectorAll(".square");
+
     squares.forEach(square => {
-        square.addEventListener("mouseover", (event) => {
+        square.addEventListener("mouseover", () => {
             square.classList.add("hovered");
-        })    
-    });
-    clear.addEventListener("click", () => {
-        squares.forEach(square => {
-            square.classList.remove("hovered");
-        });
-    });
-    
-    newGrid.addEventListener("click", () => {
-        let input = prompt("How many squares per side do you want in the new grid?");
-        squares.forEach(square => {
-            square.remove();
-        });
-        renderGrid(input);
+        });    
     });
 }
 
+// Set up event listeners
+clear.addEventListener("click", () => {
+    squares.forEach(square => {
+        square.classList.remove("hovered");
+    });
+});
+
+newGrid.addEventListener("click", () => {
+    let input = prompt("How many squares per side do you want in the new grid?");
+    squares.forEach(square => {
+        square.remove();
+    });
+    renderGrid(input);
+});
+
+// Initial render
 renderGrid(16);
